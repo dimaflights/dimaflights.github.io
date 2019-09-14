b160a06eedd6df1da376f2dc1e110822eadbce71
Tim Williams <twilliams25@gmail.com>
2019-09-14 15:43:52 -0700

    added thank you page, updated forms to connect them to CRM

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
             '<button class="delmultirow">Delete</button>'+
           '</div>'+
         '</div>';
-      var fieldHTML = '<div class="col-sm-12 nopadding triprow" id="triprow_' + multirow + '"><div class="col-sm-7 formleft"><div class="col-sm-6 datepickerinputbox"><div class="form-group"><input type="text" class="form-control flightfrom backicon locationsrch" placeholder="Flight From" value="" name="multifrom[]" id="multifrom_' + multirow + '"></div></div><div class="col-sm-6 datepickerinputbox"><div class="form-group"><input type="text" class="form-control flightto backicon locationsrch" placeholder="Flight To" value="" name="multito[]" id="multito_' + multirow + '"></div></div></div><div class="col-sm-5 formright"><div class="col-sm-5 datepickerinputbox"><input id="multitripdepart_' + multirow + '" name="multitripdepart[]" class="datepickerinput backicon" type="text" placeholder="mm/dd/yyyy" onclick="setdate(this);"/></div><div class="col-sm-4 datepickerinputbox"><select name="multitripadult_' + multirow + '" id="multitripadult_' + multirow + '"><option value="" selected disabled>Passengers</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option></select></div><div class="col-sm-3 delbtn"><button class="delmultirow">Delete</button></div></div>';
+      // var fieldHTML = '<div class="col-sm-12 nopadding triprow" id="triprow_' + multirow + '"><div class="col-sm-7 formleft"><div class="col-sm-6 datepickerinputbox"><div class="form-group"><input type="text" class="form-control flightfrom backicon locationsrch" placeholder="Flight From" value="" name="multifrom[]" id="multifrom_' + multirow + '"></div></div><div class="col-sm-6 datepickerinputbox"><div class="form-group"><input type="text" class="form-control flightto backicon locationsrch" placeholder="Flight To" value="" name="multito[]" id="multito_' + multirow + '"></div></div></div><div class="col-sm-5 formright"><div class="col-sm-5 datepickerinputbox"><input id="multitripdepart_' + multirow + '" name="multitripdepart[]" class="datepickerinput backicon" type="text" placeholder="mm/dd/yyyy" onclick="setdate(this);"/></div><div class="col-sm-4 datepickerinputbox"><select name="multitripadult_' + multirow + '" id="multitripadult_' + multirow + '"><option value="" selected disabled>Passengers</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option></select></div><div class="col-sm-3 delbtn"><button class="delmultirow">Delete</button></div></div>';
 
 
     $('body').find('.triprow:last').after(fieldHTML);
@@ -129,6 +129,83 @@ $(document).ready(function () {
     b.preventDefault(),
       $(this).toggleClass("active").next().toggleClass("active")
   })
+
+
+  function requestSuccess(response) {
+    console.log(response);
+  }
+
+  $(".flightform").on("submit", function (event) {
+    event.preventDefault();
+    var nameMap = {
+      // Round Trip
+      "roundfrom": "LEADCF1",
+      "roundto": "LEADCF2",
+      "rounddepart": "LEADCF81",
+      "roundreturn": "LEADCF83",
+      "roundpassenger": "LEADCF51",
+
+      // One Way
+      "onewayfrom": "",
+      "onewayto": "",
+      "onewaydepart": "",
+      "onewaypassenger": "LEADCF51",
+
+      // Multi Form
+      "class": "LEADCF8",
+
+      "multifrom_1": "LEADCF1",
+      "multito_1": "LEADCF2",
+      "multitripdepart_1": "LEADCF81",
+      "multitripadult_1": "LEADCF51",
+
+      "multifrom_2": "LEADCF4",
+      "multito_2": "LEADCF6",
+      "multitripdepart_2": "LEADCF85",
+      "multitripadult_2": "LEADCF51",
+
+      "multifrom_3": "LEADCF5",
+      "multito_3": "LEADCF7",
+      "multitripdepart_3": "LEADCF84",
+      "multitripadult_3": "LEADCF51",
+
+      "lastname": "Last Name",
+      "email": "Email",
+      "phone": "Phone",
+
+
+    }
+    var data = $(this).serializeArray();
+    var url = "https://crm.zoho.com/crm/WebToLeadForm";
+    var tyPage = "https&#x3a;&#x2f;&#x2f;www.flightaroo.com&#x2f;thank-you&#x2f;"
+    var postData = {
+      "xnQsjsdp": "f2bc23ae6e9abcbb676146562effba055f64c357581796f8e039d596534c7325",
+      "zc_gad": "",
+      "xmIwtLD": "5a5ba60886b11c463c081fcac4bd42c615de07f283189a74a05966c1d0e27989",
+      "actionType": "TGVhZHM=",
+      "returnURL": tyPage,
+      "LEADCF9": "Yes"
+    }
+
+    data.map(function (item) {
+      postData[nameMap[item.name]] = item.value
+    })
+
+    console.log(postData);
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: postData,
+      success: function(res) {
+        if(res) window.location.replace("./thank-you.html");
+      },
+      fail: function(err) {
+        console.log("Ajax error: ");
+        console.log(err);
+      }
+    });
+
+  });
 
 });
 
